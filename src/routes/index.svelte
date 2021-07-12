ed1c10cff26ba07f871afada177158f3e0b6456a
Peter Szerzo <szerzo.peter@gmail.com>
2021-07-12 11:36:19 +0200

    Add performance section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,6 @@
-<svelte:head>
-  <title>Anna Cingi | Set Designer</title>
-</svelte:head>
-
-<section>
-  <WingTitle title="Theatre" />
-  <Wings>
-    {#each theatreProjects as project, index}
-      <Wing project={project} />
-    {/each}
-  </Wings>
-</section>
-
-<section>
-  <WingTitle title="Opera" />
-  <Wings>
-    {#each operaProjects as project, index}
-      <Wing project={project} />
-    {/each}
-  </Wings>
-</section>
-
-<div style="display:none" aria-hidden>
-  <a href="/admin">Admin</a>
-</div>
-
 <script context="module">
   export function preload({ params, query }) {
-    return this.fetch("index.data.json").then(res => res.json());
+    return this.fetch("index.data.json").then((res) => res.json());
   }
 </script>
 
@@ -37,10 +11,51 @@
 
   export let projects;
 
-  $: theatreProjects = projects.filter(project => project.group === "Theatre");
+  $: theatreProjects = projects.filter(
+    (project) => project.group === "Theatre"
+  );
 
-  $: operaProjects = projects.filter(project => project.group === "Opera");
+  $: performanceProjects = projects.filter(
+    (project) => project.group === "Performance"
+  );
+
+  $: operaProjects = projects.filter((project) => project.group === "Opera");
 </script>
+
+<svelte:head>
+  <title>Anna Cingi | Set Designer</title>
+</svelte:head>
+
+<section>
+  <WingTitle title="Theatre" />
+  <Wings>
+    {#each theatreProjects as project, index}
+      <Wing {project} />
+    {/each}
+  </Wings>
+</section>
+
+<section>
+  <WingTitle title="Performance" />
+  <Wings>
+    {#each performanceProjects as project, index}
+      <Wing {project} />
+    {/each}
+  </Wings>
+</section>
+
+<section>
+  <WingTitle title="Opera" />
+  <Wings>
+    {#each operaProjects as project, index}
+      <Wing {project} />
+    {/each}
+  </Wings>
+</section>
+
+<div style="display:none" aria-hidden>
+  <a href="/admin">Admin</a>
+</div>
 
 <style>
   section:not(:first-child) {
