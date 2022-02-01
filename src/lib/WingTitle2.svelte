77619c6973cd86e77eafaf59e9a6e058ac8bc586
Peter Szerzo <szerzo.peter@gmail.com>
2022-02-01 17:43:30 +0100

    Remove tag link feature

--- src/lib/WingTitle2.svelte
+++ src/lib/WingTitle2.svelte
@@ -1,32 +1,29 @@
-<div class="wing-title2">
-  <h1 class="title stickout">{title}</h1>
-  {#if tags}
-    <div class="tags">
-      {#each tags as tag}
-        <Tag
-          tag={tag}
-          href="{`/tags/${tagUrl(tag)}`}"
-        />
-      {/each}
-    </div>
-  {/if}
-</div>
-
-<script>
+<script lang="ts">
   import Tag from "./Tag.svelte";
 
-  export const tagUrl = tag => {
+  export const tagUrl = (tag) => {
     return tag && tag.replace && tag.replace(/\ /g, "--");
-  }
+  };
 
   export let title;
   export let tags;
 </script>
 
+<div class="wing-title2">
+  <h1 class="title stickout">{title}</h1>
+  {#if tags}
+    <div class="tags">
+      {#each tags as tag}
+        <Tag {tag} />
+      {/each}
+    </div>
+  {/if}
+</div>
+
 <style>
   .wing-title2 {
     color: #000;
-    background-color: #FFF;
+    background-color: #fff;
     width: -moz-fit-content;
     width: fit-content;
     transform: skewY(-14deg);
