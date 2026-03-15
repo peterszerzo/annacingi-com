e1917048a9c4f960c8e51bb02e8a462902e0a09b
Peter Szerzo <szerzo.peter@gmail.com>
2026-03-15 15:31:50 +0100

    Update content

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,5 @@
 <footer>
-  Anna Cingi 2024 / Website by <a href="https://peterszerzo.com">@peterszerzo</a
+  Anna Cingi 2021-2026 / Website by <a href="http://peterszerzo.com">@peterszerzo</a
   >
 </footer>
 
