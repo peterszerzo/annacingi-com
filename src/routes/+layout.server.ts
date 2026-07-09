db676d911c16157c3bc0d61bed401e59136b8310
Peter Szerzo <szerzo.peter@gmail.com>
2026-07-09 18:09:30 +0200

    Hack CV path

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,16 @@
-import frontMatter from "front-matter";
-import { fs } from "mz";
+// import frontMatter from "front-matter";
+// import { fs } from "mz";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-  const content = (await fs.readFile("static/cms/cv.md")).toString();
-
   return {
-    cvUrl: frontMatter<{ cvEnglish: string }>(content).attributes.cvEnglish,
+    cvUrl: "/cms/images/annacingi-engcv-jul26.pdf",
   };
+  // const content = (await fs.readFile("static/cms/cv.md")).toString();
+
+  // return {
+  //   cvUrl: frontMatter<{ cvEnglish: string }>(content).attributes.cvEnglish,
+  // };
 };
 
 export const prerender = true;
